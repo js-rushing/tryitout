feec5facbaef46d779e55294c7c25bebb9a3b4f0
js-rushing <unoriginal9000@yahoo.com>
2020-06-23 17:04:41 -0500

    Just tryin it out

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('page-title').innerHTML = "Try It Out";
+document.getElementById('left-h2').innerHTML = "Left Side";
+document.getElementById('right-h2').innerHTML = "Right Side";
+document.getElementById('left-para').innerHTML = "Left Side sample paragraph";
+document.getElementById('right-para').innerHTML = "Right Side sample paragraph";
